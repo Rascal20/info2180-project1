87f3f2a8718318d18b787ee6a03cfe9161bad141
Rene Tim <rdatim20@gmail.com>
2021-10-24 11:56:04 -0400

    Subscribe button functionality implemented

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
 /* Add your JavaScript to this file */
+var run = function(){
+    var getEmail = document.getElementById("email");
+    var btnClick = document.getElementsByClassName("btn")[1].onclick
+    btnClick = function(clickEvent){
+        clickEvent.preventDefault();
+        var email = getEmail.value;
+        if (email == ""){
+            document.getElementsByClassName("message")[0].innerHTML = "Please enter a valid email address.";
+        }
+        else{
+            document.getElementsByClassName("message")[0].innerHTML = "Thank you! Your email address " + email + " has been added to our mailing list!";
+        }
+    }
+}
+window.onload = run();
